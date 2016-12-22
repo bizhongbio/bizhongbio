9682f90058ab9f79eef13d9343b81b55cd177751
bizhong <bizhonglan@gmail.com>
2016-12-22 23:59:00 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <aside class="sidebar">
                 <article class="promos">
-                    <a href="https://github.com/bizhong" title="bizhong (Billy Lan) · GitHub" target="_blank"><i class="fa fa-github"></i> GitHub</a>
+                    <a href="https://github.com/bizhong" title="bizhong (Bizhong Lan) · GitHub" target="_blank"><i class="fa fa-github"></i> GitHub</a>
                 </article>
                 <section class="recommended-posts">
                     <h2>推荐文章</h2>
@@ -22,7 +22,7 @@
                     <h2>友情链接</h2>
                     <ul>
                         <!-- 在这里添加友情链接 -->
-                        <li><a href="http://www.bizhongbio.com/" title="双鱼bizhongbio - 渡人渡己，前端开发技术博客" target="_blank">双鱼bizhongbio</a></li>
+                        <li><a href="https://bizhongbio.github.io/" title="双鱼bizhongbio - 渡人渡己，前端开发技术博客" target="_blank">双鱼bizhongbio</a></li>
                     </ul>
                 </section>
             </aside>
