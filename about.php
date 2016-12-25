f60c9dfa0f0e09280981caf51997ff0daf5a9f98
bizhong <bizhonglan@gmail.com>
2016-12-25 17:37:22 +0800

    Update about.php

--- about.php
+++ about.php
@@ -8,13 +8,13 @@ Template Name:关于
         <div class="content">
             <section class="about">
                 <h2>双鱼bizhongbio</h2>
-                <p>2014 年 03 月，我有幸加入中南民族大学 <a href="http://www.new-thread.com/" title="新思路团队网站 - NewThread">NewThread 团队创新实验室</a> Feweb 组学习前端开发。一名正走在前端开发工程师（FED）路上的程序猿。工作联系（E-mail）：<a href="mailto:bizhongbio@outlook.com" target="_blank">bizhongbio@outlook.com</a></p>
+                <p>2014 年 03 月，我有幸加入中南民族大学 <a href="http://www.new-thread.com/" title="新思路团队网站 - NewThread">NewThread 团队创新实验室</a> Feweb 组学习前端开发。一名正走在前端开发工程师路上的程序猿。邮箱：<a href="mailto:bizhongbio@outlook.com" target="_blank">bizhongbio@outlook.com</a></p>
                 <div class="aboutme">
-                    <img src="<?php bloginfo('url'); ?>/images/bizhongbio.png" alt="双鱼bizhongbio">
+                    <img src="<?php bloginfo('url'); ?>/images/about.png" alt="双鱼bizhongbio">
                 </div>
                 <div class="social">
-                    <a href="https://github.com/bizhong" title="bizhong (Billy Lan) · GitHub" target="_blank"><i class="fa fa-github"></i></a>
-                    <a href="https://www.behance.net/bizhong" title="Billy Lan on Behance" target="_blank"><i class="fa fa-behance"></i></a>
+                    <a href="https://github.com/bizhong" title="bizhong (Bizhong Lan) · GitHub" target="_blank"><i class="fa fa-github"></i></a>
+                    <a href="https://www.behance.net/bizhong" title="Bizhong Lan on Behance" target="_blank"><i class="fa fa-behance"></i></a>
                     <a href="http://weibo.com/bizhongbio" title="兰必钟的微博" target="_blank"><i class="fa fa-weibo"></i></a>
                 </div>
             </section>
